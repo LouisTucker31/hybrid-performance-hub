a8ea5204c3b0b50876a9310e0893fb4c40cd5ea2
LouisTucker31 <louistucker311097@gmail.com>
2026-07-28 09:10:05 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -19,10 +19,10 @@
       <ul class="nav-links" id="nav-links">
         <li><a href="./" data-page="home">Home</a></li>
         <li><a href="about/" data-page="about">About</a></li>
-        <li><a href="bespoke/" data-page="bespoke">Bespoke</a></li>
-        <li><a href="plans/" data-page="plans">Plans</a></li>
         <li><a href="tools/" data-page="tools">Tools</a></li>
         <li><a href="blog/" data-page="blog">Blog</a></li>
+        <li><a href="bespoke/" data-page="bespoke">Bespoke</a></li>
+        <li><a href="plans/" data-page="plans">Plans</a></li>
         <li><a href="contact/" data-page="contact">Contact</a></li>
       </ul>
     </nav>
@@ -35,10 +35,10 @@
     <ul class="footer-nav">
       <li><a href="./">Home</a></li>
       <li><a href="about/">About</a></li>
-      <li><a href="bespoke/">Bespoke</a></li>
-      <li><a href="plans/">Plans</a></li>
       <li><a href="tools/">Tools</a></li>
       <li><a href="blog/">Blog</a></li>
+      <li><a href="bespoke/">Bespoke</a></li>
+      <li><a href="plans/">Plans</a></li>
       <li><a href="contact/">Contact</a></li>
       <li><a href="privacy/">Privacy Policy</a></li>
       <li><a href="terms/">Terms of Service</a></li>
